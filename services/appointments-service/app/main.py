cb3f4ea324d2f0b996d12f8f439d7d3bfb93ba3e
Juan UC <jnocuac@ucentral.edu.co>
2026-04-03 15:22:34 -0500

    Agregar URL de static page de Render al CORS

--- services/appointments-service/app/main.py
+++ services/appointments-service/app/main.py
@@ -66,7 +66,8 @@ app.add_middleware(
         "http://localhost:5173",      # Vite dev
         "http://localhost:3000",      # React alternativo
 		"http://127.0.0.1:5173",
-        "https://eps-digital.onrender.com",  # Frontend en Render
+        "https://eps-digital.onrender.com",
+		"https://eps-digital-cn2h.onrender.com",  # Frontend en Render
     ],
     allow_credentials=True,
     allow_methods=["*"],
